80041efd5a977a90ba928e45226a8b4510703cdf
Marco Alberti <malbertife@gmail.com>
2022-09-30 06:22:25 +0200

    soluzioni eseritazione 29 settembre 2022

--- codice/l01/differenza-fra-date.c
+++ codice/l01/differenza-fra-date.c
@@ -3,34 +3,33 @@
 #include <stdio.h>
 
 main() {
-  int G1, M1, A1;
-  int G2, M2, A2;
+  int G, M, A;
   int N0, N1, N2, N3;
   int JD1, JD2;
 
   printf("Inserisci giorno, mese e anno della prima data\n");
-  scanf("%d%d%d", &G1, &M1, &A1);
+  scanf("%d%d%d", &G, &M, &A);
 
-  N0 = (M1 - 14) / 12;
-  N1 = (1461 * (A1 + 4800 + N0)) / 4;
-  N2 = (367 * (M1 - 2 - 12 * N0)) / 12;
-  N3 = (3 * (A1 + 4900 + N0)) / 400;
+  N0 = (M - 14) / 12;
+  N1 = (1461 * (A + 4800 + N0)) / 4;
+  N2 = (367 * (M - 2 - 12 * N0)) / 12;
+  N3 = (3 * (A + 4900 + N0)) / 400;
 
-  JD1 = N1 + N2 - N3 + G1 - 32075;
+  JD1 = N1 + N2 - N3 + G - 32075;
 
   // input della seconda data
 
   printf("Inserisci giorno, mese e anno della seconda data\n");
-  scanf("%d%d%d", &G2, &M2, &A2);
+  scanf("%d%d%d", &G, &M, &A);
 
   // calcoli
 
-  N0 = (M2 - 14) / 12;
-  N1 = (1461 * (A2 + 4800 + N0)) / 4;
-  N2 = (367 * (M2 - 2 - 12 * N0)) / 12;
-  N3 = (3 * (A2 + 4900 + N0)) / 400;
+  N0 = (M - 14) / 12;
+  N1 = (1461 * (A + 4800 + N0)) / 4;
+  N2 = (367 * (M - 2 - 12 * N0)) / 12;
+  N3 = (3 * (A + 4900 + N0)) / 400;
 
-  JD2 = N1 + N2 - N3 + G2 - 32075;
+  JD2 = N1 + N2 - N3 + G - 32075;
 
   // stampa differenza
   printf("La differenza e` %d\n", JD2 - JD1);
---
+++ codice/l01/giorno-della-settimana.c
@@ -0,0 +1,32 @@
+// calcola e stampa il giorno della settimana della data in input
+// 0 = lun, 1 = mar, ..., 6 = dom
+#include <stdio.h>
+
+main() {
+  // definizioni
+  int G, M, A;
+  int N0, N1, N2, N3;
+  int JD;
+
+  // input
+  printf("Inserisci giorno, mese e anno di una data\n");
+  scanf("%d", &G);
+  scanf("%d", &M);
+  scanf("%d", &A);
+
+  // calcolo risultati intermedi
+  N0 = (M - 14) / 12;
+  N1 = (1461 * (A + 4800 + N0)) / 4;
+  N2 = (367 * (M - 2 - 12 * N0)) / 12;
+  N3 = (3 * (A + 4900 + N0)) / 400;
+  // calcolo risultato finale
+  JD = N1 + N2 - N3 + G - 32075;
+  // stampa risultato finale
+
+  printf("Il giorno della settimana e` %d\n", JD % 7);
+}
+
+// Indentazione automatica:
+// Windows Alt+Shift+F
+// Linux Ctrl+Shift+I
+// Mac Shift+Option+F
