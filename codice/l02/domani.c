f6b8e89b58175937d79f6b1d288ba84c021281a8
Marco Alberti <malbertife@gmail.com>
2022-10-06 14:23:24 +0200

    traccia domani.c

---
+++ codice/l02/domani.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main(){
+    int giorno, mese, anno;
+    scanf("%d%d%d", &giorno, &mese, & anno);
+    // se fine anno (es. 31 12 2022)
+    // 1 1 anno+1
+    // altrimenti se fine mese (es. 28 2 2022)
+    // 1 mese+1 anno
+    // altrimenti (es. 6 10 2022)
+    // giorno+1 mese anno
+
+}
