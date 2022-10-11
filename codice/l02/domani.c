d481f1e91e38d163bf92caed917322c641592122
Marco Alberti <malbertife@gmail.com>
2022-10-11 10:54:44 +0200

    domani

--- codice/l02/domani.c
+++ codice/l02/domani.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
 
-main(){
-    int giorno, mese, anno;
-    scanf("%d%d%d", &giorno, &mese, & anno);
-    // se fine anno (es. 31 12 2022)
-    // 1 1 anno+1
-    // altrimenti se fine mese (es. 28 2 2022)
-    // 1 mese+1 anno
-    // altrimenti (es. 6 10 2022)
-    // giorno+1 mese anno
+main() {
+  int giorno, mese, anno, n_giorni;
+  printf("Inserisci giorno, mese e anno di una data\n");
+  scanf("%d%d%d", &giorno, &mese, &anno);
 
+  if (mese == 2)
+    if (anno % 4 == 0 && anno % 100 != 0 || anno % 400 == 0)
+      n_giorni = 29;
+    else
+      n_giorni = 28;
+  else if (mese == 4 || mese == 6 || mese == 9 || mese == 11)
+    n_giorni = 30;
+  else
+    n_giorni = 31;
+  // se fine anno (es. 31 12 2022)
+
+  if (giorno < 1 || giorno > n_giorni)
+    printf("Data non esistente\n");
+  else if (giorno == 31 && mese == 12)
+    // 1 1 anno+1
+    printf("%d/%d/%d\n", 1, 1, anno + 1);
+  // altrimenti se fine mese (es. 28 2 2022)
+  else if (giorno == n_giorni)
+    // 1 mese+1 anno
+    printf("%d/%d/%d\n", 1, mese + 1, anno);
+  // altrimenti (es. 6 10 2022)
+  // giorno+1 mese anno
+  else
+    printf("%d/%d/%d\n", giorno + 1, mese, anno);
 }
