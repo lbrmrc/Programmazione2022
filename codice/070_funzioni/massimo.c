4596c76c68d3003141774a397e7940ab6cad461d
Marco Alberti <malbertife@gmail.com>
2022-10-26 12:32:07 +0200

    massimo

---
+++ codice/070_funzioni/massimo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int massimo(int a, int b) {
+  if (a > b)
+    return a;
+  else
+    return b;
+  // return m > n ? m : n;
+}
+
+int main() {
+  int a, b;
+  printf("inserisci due interi\n");
+  scanf("%d%d", &a, &b);
+  printf("Il massimo è %d\n", massimo(a, b));
+}
