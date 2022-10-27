cc483f1e2b09d63d83434489406d442a7ff15449
Marco Alberti <malbertife@gmail.com>
2022-10-27 11:27:50 +0200

    correzione a radice quadrata

--- codice/l05/radice-quadrata.c
+++ codice/l05/radice-quadrata.c
@@ -5,12 +5,12 @@ int main(void) {
   float a, x;
   printf("Digita un numero\n");
   scanf("%f", &a);
-  if (a < 0) {
-    printf("%f e` negativo\n", a);
+  if (a <= 0) {
+    printf("%f e` negativo o nullo\n", a);
     return -1;
   }
   x = 1.0;
-  while (fabsf(x * x - a) > 1e-5)
+  while (fabsf(x * x - a) / a > 1e-5)
     x = (x + a / x) / 2;
   printf("La radice quadrata di %.2f e` circa %f\n", a, x);
   return 0;
