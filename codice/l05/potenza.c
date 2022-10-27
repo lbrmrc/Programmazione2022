cb60927e49df8d1449dde065be56e1de96de12ea
Marco Alberti <malbertife@gmail.com>
2022-10-27 11:12:56 +0200

    potenza versione 1

--- codice/l05/potenza.c
+++ codice/l05/potenza.c
@@ -1,6 +1,27 @@
-int potenza(int base, int esp) {
-  int cont, prod = 1;
-  for (cont = 0; cont < esp; cont++)
-    prod *= base;
-  return prod;
+#include <stdio.h>
+
+float potenza(float base, int esp) {
+  int cont;
+  float prod;
+  if (esp >= 0) {
+    prod = 1.0;
+    for (cont = 0; cont < esp; cont++)
+      prod *= base;
+    return prod;
+  } else {
+    esp = -esp;
+    prod = 1.0;
+    for (cont = 0; cont < esp; cont++)
+      prod *= base;
+    return 1.0/prod;
+  }
+}
+
+int main() {
+  int e;
+  float b;
+  printf("Inserisci base (reale) ed esponente (intero con segno)\n");
+  scanf("%f%d", &b, &e);
+  printf("%f\n", potenza(b, e));
+  return 0;
 }
