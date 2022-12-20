32773e4bc56e7adf08d1d5262d10dfe5cc971b06
Marco Alberti <malbertife@gmail.com>
2022-12-20 11:14:19 +0100

    list to array

--- codice/230_liste_collegate/listToArray.c
+++ codice/230_liste_collegate/listToArray.c
@@ -8,24 +8,46 @@ typedef struct nodo {
 } Nodo;
 typedef Nodo* Lista;
 
-#include "generatoreListe.h"
-
-int lunghezza(Lista l) {
-  int c = 0;
-  while (l) {
-    c++;
-    l = l->next;
+// fa puntare pl a una lista non ordinata di n elementi (n <= 10)
+void listaNonOrdinata(Lista* pl, int n) {
+  // gli elementi da inserire nella lista
+  int a[] = {6, 2, 3, 2, 4, 7, 0, 2, 5, 1};
+  int i;
+  // per i che va da 0 a n - 1
+  for (i = 0; i < n; i++) {
+    // *pl punta a un nuovo nodo
+    (*pl) = (Nodo*)malloc(sizeof(Nodo));
+    // il cui dato è a[i]
+    (*pl)->dato = a[i];
+    // e il cui campo next è NULL (cioè *pl ha un solo elemento; eventualmente
+    // ne saranno aggiunti altri nelle iterazioni successive)
+    (*pl)->next = NULL;
+    // assegna a pl l'indirizzo della sua coda
+    pl = &(*pl)->next;
   }
-  return c;
 }
 
-int* listToArray(Lista l) {
-  int *p, i;
-  int n = lunghezza(l);
-  p = (int*)malloc(sizeof(int) * n);
-  // ForEach (assegna elemento a elemento i++-esimo dell'array, l)
+int lunghezza(Lista l) {
+  int cont = 0;
+  while (l != NULL) {
+    cont++;
+    l = l->next;
+  }
+  return cont;
+}
+
+int* list_to_array(Lista l) {
+  int dimensione;
+  int* p;
+  int i;
+  dimensione = lunghezza(l);
+  p = (int*)malloc(dimensione * sizeof(int));
+  if (p == NULL){ 
+    printf("Errore allocazione memoria\n");
+    exit(1);
+  }
   i = 0;
-  while (l) {
+  while (l != NULL) {
     p[i] = l->dato;
     i++;
     l = l->next;
@@ -35,12 +57,14 @@ int* listToArray(Lista l) {
 
 int main() {
   Lista l;
-  int *p, i, n;
+  int* a;
+  int i;
+  int dimensione = lunghezza(l);
   listaNonOrdinata(&l, 6);
-  p = listToArray(l);
-  n = lunghezza(l);
-  for (i = 0; i < n; i++) {
-    printf("%d\n", p[i]);
-  }
+
+  a = list_to_array(l);
+  for (i = 0; i < dimensione; i++)
+    printf("%d ", a[i]);
+  printf("\n");
   return 0;
 }
